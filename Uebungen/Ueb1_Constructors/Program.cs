c98bceb260adc5ca8d56737141e4be197f05d59b
Buriii <buri@github.com>
2018-02-26 23:11:36 +0100

    Initial commit

---
+++ Uebungen/Ueb1_Constructors/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Ueb1
+{
+    public class ClassA
+    {        
+        public ClassA()//Default Konstruktor
+        {
+
+        }
+
+        public ClassA(string msg)
+        {
+            Console.WriteLine("\nA:" + msg + " "); 
+        }
+    }
+
+    //**************************************************************
+    public class ClassB: ClassA
+    {
+        public ClassB(string msg, int count)
+        {
+            Console.Write("B:" + msg); 
+
+            for(int i = 0; i<count; i++)
+            {
+                Console.Write(".");
+            }
+            Console.Write(" "); 
+        }
+
+        public ClassB(string msg):this(msg, 1){}
+
+        public ClassB():this("B0", 0) {}
+    }
+
+    //**************************************************************
+    public class Application
+    {
+        static void Main(string[] args)
+        {
+            ClassA A = new ClassA("A");
+            ClassB B0 = new ClassB();
+            ClassB B1 = new ClassB("B1");
+            ClassB B2 = new ClassB("B2", 2);
+
+            ClassA AA = new ClassA("AA");
+            ClassA AB = new ClassB("AB");
+
+            Console.Read();
+        }
+    }
+    
+}
+
+
---
+++ Uebungen/Ueb1_HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ueb1_HelloWorld
+{
+    class Program
+    {
+        private static void PrintMessage(string msg, bool upperCase)
+        {
+            if(upperCase)
+            {
+               msg = msg.ToUpper(); 
+            }
+            Console.WriteLine(msg); 
+        }
+
+        static void Main(string[] args)
+        {
+            PrintMessage("Hello World", false);
+            PrintMessage("hello World", true); 
+
+            Console.Read(); 
+        }
+    }
+}
